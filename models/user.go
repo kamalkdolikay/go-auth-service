15459770a991d9f7ce2cb9ac26522505dfafce43
Kamal Dolikay <kamaldolikay@gmail.com>
2025-11-23 18:04:11 -0800

    update(schema): add role, createdat in user schema

--- models/user.go
+++ models/user.go
@@ -7,5 +7,6 @@ type User struct {
 	Name      string    `json:"name"`
 	Email     string    `json:"email"`
 	Password  string    `json:"password,omitempty"`
-	CreatedAt time.Time `json:"created_at,omitempty"`
+	Role      string    `json:"role"`
+	CreatedAt time.Time `json:"created_at"`
 }
